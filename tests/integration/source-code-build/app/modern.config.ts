3c3c299bb73b7c70be4670940c121b64541242c4
targeral <targeral@outlook.com>
2023-07-31 18:34:45 +0800

    test: add tsconfig-paths-webpack-plugin in source code build test case (#4338)

--- tests/integration/source-code-build/app/modern.config.ts
+++ tests/integration/source-code-build/app/modern.config.ts
@@ -1,4 +1,9 @@
+import path from 'path';
 import appTools, { defineConfig } from '@modern-js/app-tools';
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+
+const tsconfigFile = path.join(__dirname, './tsconfig.json');
+console.info('tsconfigFile', tsconfigFile);
 
 export default defineConfig({
   runtime: {
@@ -14,5 +19,24 @@ export default defineConfig({
   ],
   experiments: {
     sourceBuild: true,
+  },
+  tools: {
+    webpackChain(chain) {
+      chain.resolve
+        .plugin('tsconfig-paths-webpack-plugin')
+        .use(TsconfigPathsPlugin, [
+          {
+            configFile: tsconfigFile,
+            logLevel: 'INFO',
+            extensions: chain.resolve.extensions.values(),
+            mainFields: ['browser', 'module', 'main'],
+            references: [
+              '../components/tsconfig.json', // 直接依赖：app->components
+              '../utils/tsconfig.json', // 间接依赖：app->components->utils
+            ],
+            // baseUrl: "/foo"
+          },
+        ]);
+    },
   },
 });
